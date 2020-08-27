5b4b678373e5d353916b98c1a020b59bb34ed75a
Nataliya <an.chishevich@yandex.ru>
2020-08-27 17:21:06 +0300

    Additional changes

--- NataliyaGlot/homework02/main/test/spec/converter_spec.rb
+++ NataliyaGlot/homework02/main/test/spec/converter_spec.rb
@@ -2,47 +2,38 @@ require 'rspec'
 require 'converter'
 
 RSpec.describe Converter do
-  describe '.to_eur' do
-    context 'testing the Converter class' do
-      it 'should display currency conversion in euro' do
-        converter = Converter.new('1.00', 'USD')
-        converter.to_eur
-      end
+  describe Converter do
+    before { @converter = Converter.new('1.00', 'USD') }
+    it 'should display currency conversion in eur' do
+      expect(@converter.to_eur).to eq(0.85)
     end
   end
 
-  describe '.to_usd' do
-    context 'testing the Converter class' do
-      it 'should display currency conversion in usd' do
-        converter = Converter.new('1.00', 'EUR')
-        converter.to_usd
-      end
+  describe Converter do
+    before { @converter = Converter.new('1.00', 'EUR') }
+    it 'should display currency conversion in usd' do
+      expect(@converter.to_usd).to eq(1.18)
     end
   end
 
-  describe '.to_rus' do
-    context 'testing the Converter class' do
-      it 'should display currency conversion in rus' do
-        converter = Converter.new('1.00', 'USD')
-        converter.to_rus
-      end
+  describe Converter do
+    before { @converter = Converter.new('1.00', 'USD') }
+    it 'should display currency conversion in rus' do
+      expect(@converter.to_rus).to eq(74.17)
     end
   end
 
-  describe '.to_byn' do
-    context 'testing the Converter class' do
-      it 'should display currency conversion in byn' do
-        converter = Converter.new('1.00', 'USD')
-        converter.to_byn
-      end
+  describe Converter do
+    before { @converter = Converter.new('1.00', 'USD') }
+    it 'should display currency conversion in byn' do
+      expect(@converter.to_byn).to eq(2.45)
     end
   end
 
-  describe '.convert' do
-    context 'testing the Converter class' do
-      it 'should display currency conversion in euro' do
-        Converter.convert('1.00', 'USD', 'EUR')
-      end
+  describe Converter do
+    before { @converter = Converter.convert('1.00', 'USD', 'EUR') }
+    it 'should display currency conversion in eur' do
+      expect(@converter).to eq(0.85)
     end
   end
 end
