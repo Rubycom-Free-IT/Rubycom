f5f5c96bfbacb8490a972db3f59bb8887c654cd8
Nataliya <an.chishevich@yandex.ru>
2020-09-02 00:21:13 +0300

    Adjustment

--- NataliyaGlot/homework02/converter/lib/converter.rb
+++ NataliyaGlot/homework02/converter/lib/converter.rb
@@ -15,65 +15,47 @@ class Converter
     rescue StandardError
       puts 'File not found or damaged'
     end
-    @file_hash = JSON.parse(file_path)
-    @cur_rate_eur = @file_hash['currency'][0]['cur_rate']
-    @cur_rate_usd = @file_hash['currency'][1]['cur_rate']
-    @cur_scale_rus = @file_hash['currency'][2]['cur_scale']
-    @cur_rate_rus = @file_hash['currency'][2]['cur_rate']
+    file_hash = JSON.parse(file_path)
+    @cur_rate_eur = file_hash['currency'][0]['cur_rate']
+    @cur_rate_usd = file_hash['currency'][1]['cur_rate']
+    @cur_rate_rus = file_hash['currency'][2]['cur_rate']
+    @cur_scale_rus = file_hash['currency'][2]['cur_scale']
+    
     if cur_name.nil? || cur_name == '' || cur_name.is_a?(Integer) || cur_name == cur_name.downcase
       raise ArgumentError, 'Currency name entered incorrectly'
-    end
-    if num_conv.nil? || num_conv == '' || num_conv.is_a?(Integer)
+    elsif num_conv.nil? || num_conv == '' || num_conv.is_a?(Integer)
       raise ArgumentError, 'Currency amount entered incorrectly'
+    elsif cur_name == 'RUS' && num_conv < '100'
+      raise ArgumentError, 'Currency must be greater than 100'
     end
   end
 
   def to_eur
-    if cur_name == 'BYN'
-      (@num_conv.to_i / @cur_rate_eur).round(2)
-    elsif cur_name == 'USD'
-      (@num_conv.to_i * @cur_rate_usd / @cur_rate_eur).round(2)
-    elsif cur_name == 'RUS'
-      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_eur).round(2)
-    else
-      'Currency not found'
-    end
+    cur_name == 'BYN' ? (num_conv.to_i / @cur_rate_eur).round(2) : 
+    cur_name == 'USD' ? (to_byn / @cur_rate_eur).round(2) :
+    cur_name == 'RUS' ? (to_byn / @cur_rate_eur).round(2) :
+    'Currency not found'
   end
 
   def to_usd
-    if cur_name == 'BYN'
-      (@num_conv.to_i / @cur_rate_usd).round(2)
-    elsif cur_name == 'EUR'
-      (@num_conv.to_i * @cur_rate_eur / @cur_rate_usd).round(2)
-    elsif cur_name == 'RUS'
-      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus / @cur_rate_usd).round(2)
-    else
-      'Currency not found'
-    end
+    cur_name == 'BYN' ? (num_conv.to_i / @cur_rate_usd).round(2) :
+    cur_name == 'EUR' ? (to_byn / @cur_rate_usd).round(2) :
+    cur_name == 'RUS' ? (to_byn / @cur_rate_usd).round(2) :
+    'Currency not found'
   end
 
   def to_rus
-    if cur_name == 'BYN'
-      (@num_conv.to_i * @cur_scale_rus / @cur_rate_rus).round(2)
-    elsif cur_name == 'USD'
-      (@num_conv.to_i * @cur_rate_usd * @cur_scale_rus / @cur_rate_rus).round(2)
-    elsif cur_name == 'EUR'
-      (@num_conv.to_i * @cur_rate_eur * @cur_scale_rus / @cur_rate_rus).round(2)
-    else
-      'Currency not found'
-    end
+    cur_name == 'BYN' ? (num_conv.to_i * @cur_scale_rus / @cur_rate_rus).round(2) :
+    cur_name == 'USD' ? (to_byn * @cur_scale_rus / @cur_rate_rus).round(2) :
+    cur_name == 'EUR' ? (to_byn * @cur_scale_rus / @cur_rate_rus).round(2) :
+    'Currency not found'
   end
 
   def to_byn
-    if @cur_name == 'EUR'
-      (@num_conv.to_i * @cur_rate_eur).round(2)
-    elsif @cur_name == 'USD'
-      (@num_conv.to_i * @cur_rate_usd).round(2)
-    elsif @cur_name == 'RUS' && @num_conv.to_i >= 100
-      (@num_conv.to_i / @cur_scale_rus * @cur_rate_rus).round(2)
-    else
-      'Currency not found or must be >= 100'
-    end
+    cur_name == 'EUR' ? (num_conv.to_i * @cur_rate_eur).round(2) :
+    cur_name == 'USD' ? (num_conv.to_i * @cur_rate_usd).round(2) :
+    cur_name == 'RUS' ? (num_conv.to_i / @cur_scale_rus * @cur_rate_rus).round(2) :
+    'Currency not found'
   end
 
   def self.convert(num_conv, cur_from, cur_to)
@@ -82,3 +64,6 @@ class Converter
     converter.public_send(:"to_#{cur_to.downcase}")
   end
 end
+
+converter = Converter.new('10', 'EUR')
+puts converter.to_rus 
